d02b1874dc75b2af0a34166b12eda55106dc459f
aiznaz <131270076+aiznaz@users.noreply.github.com>
2023-06-21 04:34:51 +0300

    1st try

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,9 +5,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-
-    private final Egg6 egg;
     @Autowired
+    private final Egg6 egg;
     public Duck5(Egg6 egg) {
         this.egg = egg;
     }
